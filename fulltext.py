4582d7977391ff45d70472ed5a8bb9e76484c128
FightLightDiamond <i.am.m.cuong@gmail.com>
2023-09-29 22:51:08 +0700

    add docker ao loi

---
+++ fulltext.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy, get_debug_queries
+
+import flask_whooshalchemy as wa
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG'] = True
+FLASKY_SLOW_DB_QUERY_TIME = 0.5
+db = SQLAlchemy(app)
+
+
+@app.get('/')
+def index():
+    return "HELLO "
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'<Course: {self.name}>'
+
+
--- model.py
+++ model.py
@@ -57,27 +57,3 @@ class Channel(db.Model):
     def __repr__(self):
         return f'<Channel: {self.name}>'
 
-
-def sql_debug(response):
-    queries = list(get_debug_queries())
-    query_str = ''
-    total_duration = 0.0
-    for q in queries:
-        total_duration += q.duration
-        stmt = str(q.statement % q.parameters).replace('\n', '\n       ')
-        query_str += 'Query: {0}\nDuration: {1}ms\n\n'.format(stmt, round(q.duration * 1000, 2))
-
-    print('=' * 80)
-    print(' SQL Queries - {0} Queries Executed in {1}ms'.format(len(queries), round(total_duration * 1000, 2)))
-    print('=' * 80)
-    print(query_str.rstrip('\n'))
-    print('=' * 80 + '\n')
-    return response
-
-
-if app.debug:
-    app.after_request(sql_debug)
-
-
-if __name__ in "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, load_dotenv=True, use_reloader=True)
